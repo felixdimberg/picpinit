624a358342ad3bc640d84aadfac7b501d66e14bb
felixdimberg <36541059+felixdimberg@users.noreply.github.com>
2020-11-13 18:35:19 +0100

    Update signup.js
    Made the page scrollable

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -65,7 +65,8 @@ class signup extends Component {
     const {classes, loading} = this.props;
     const { errors } = this.state;
     return (
-      <div style={{position: 'fixed', height: '100%', width: '100%', backgroundSize: 'cover', backgroundImage: `url(${Background})`}}>
+      <div style={{backgroundAttachment:'fixed',backgroundSize: 'cover',
+          backgroundImage: `url(${Background})`}}>
       <Grid container className= {classes.form}>
         <Grid item sm/>
         <Grid item sm>
